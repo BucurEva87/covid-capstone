e572998da39a8cfe134791b60081864401e7e95b
BucurEva87 <bucur.eva87@gmail.com>
2022-10-22 17:52:46 +0300

    Add Redux actions and store

---
+++ src/redux/countries/countries.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_BASE_URL = 'https://covid-api.mmediagroup.fr/v1';
+
+export const fetchCountriesThunk = createAsyncThunk('countries/fetch', async () => {
+  const response = await fetch(`${API_BASE_URL}/cases`);
+  const result = await response.json();
+
+  return result;
+});
+
+const countriesSlice = createSlice({
+  name: 'countries',
+  initialState: {
+    loading: false,
+    list: [],
+    error: '',
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountriesThunk.pending, (state) => ({
+      ...state, loading: true,
+    }));
+    builder.addCase(fetchCountriesThunk.fulfilled, (state, action) => {
+      const countries = {};
+
+      Object.entries(action.payload).forEach(([key, value]) => {
+        if (value.All && value.All.country) countries[key] = value.All;
+      });
+
+      return {
+        ...state,
+        loading: false,
+        list: countries,
+      };
+    });
+    builder.addCase(fetchCountriesThunk.rejected, (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }));
+  },
+});
+
+export default countriesSlice.reducer;
+export const { selectCountry } = countriesSlice.actions;
