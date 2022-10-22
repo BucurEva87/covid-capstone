26555ae761673bebcd1ce85059dafda0c1b2b1b1
BucurEva87 <bucur.eva87@gmail.com>
2022-10-22 17:57:10 +0300

    Add index.scss file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 
+import 'index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
